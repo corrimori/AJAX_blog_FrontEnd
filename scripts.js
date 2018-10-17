de3497417182355aacc81ad7306148bc7f2ed78f
corrimori <corrine@promptt.com>
2018-10-16 23:19:23 -0700

    add list of blog entries from db to display

--- scripts.js
+++ scripts.js
@@ -1,16 +1,41 @@
-const load = () => {
-  console.log("load event detected!");
-  // get all blog posts
+const loadEntries = () => {
+  console.log("load event detected...")
+
+  let list = document.querySelector('#entryList')
+  list.innerHTML = ''
+
+  axios.get('http://localhost:3001/blogs').then(result => {
+    let data = result.data
+    data.forEach( element => {
+      // console.log(element.title)
+      let listItem = document.createElement('li')
+      listItem.innerHTML = element.title
+      console.log(listItem)
+      list.appendChild(listItem)
+      // element.addEventListener('click', () =>{
+      //   li.remove()
+      })
+    })
 }
-window.onload = load;
 
 function showForm() {
   console.log('in showForm function')
   let createPostForm = document.getElementById('entryForm')
-  if (createPostForm.style.display === 'none') {
-    console.log('form>>', createPostForm.style.display);
-    createPostForm.style.display = 'block'
-  } else {
-    createPostForm.style.display = 'none'
-  }
+  createPostForm.style.display = 'block'
+  // if (createPostForm.style.display === 'none') {
+  //   console.log('form>>', createPostForm.style.display);
+  //   createPostForm.style.display = 'block'
+  // } else {
+  //   createPostForm.style.display = 'none'
+  // }
 }
+
+
+
+
+document.addEventListener('DOMContentLoaded', loadEntries)
+
+
+
+// let button = document.querySelector('#the-button')
+// button.addEventListener('click', onButtonClick)
