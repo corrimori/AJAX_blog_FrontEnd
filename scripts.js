dabef7edaa843b6f651317d2113c678aab8c4e4b
corrimori <corrine@promptt.com>
2018-10-23 13:44:33 -0700

    show blog entry on clicking entry

--- scripts.js
+++ scripts.js
@@ -10,13 +10,35 @@ const loadEntries = () => {
       // console.log(element.title)
       let listItem = document.createElement('li')
       listItem.innerHTML = element.title
-      console.log(listItem)
-      list.appendChild(listItem)
+      console.log('listitem>', listItem)
+      list.appendChild(listItem).addEventListener('click', () => {
+        console.log('clicked li')
+        let listId = list.id
+        console.log('element id>>>', element.id);
+        console.log('data>>>>', data)
+        // onclick hide showEntry
+        let displayBlogArea = document.getElementById('displayBlog')
+        displayBlogArea.style.display = 'none'
+        // when clicked, hide current and show clicked entry
+        //get titleEntry & contentEntry to pass into params
+        let titleEntry = element.title
+        let contentEntry = element.content
+        showEntry(titleEntry, contentEntry)
+      })
       // element.addEventListener('click', () =>{
       //   li.remove()
       })
     })
 }
+
+
+
+// when listitem clicked
+// display/show entry on right
+
+// when delete
+// get id of list listitem
+// axios call to remove item
 
 const showForm = () => {
   console.log('in showForm function')
@@ -30,15 +52,21 @@ const showForm = () => {
   // }
 }
 
+//function to show Blog Entry once submit button is pressed
 const showEntry = (titleEntry, contentEntry) => {
   console.log('in showEntry function')
   let displayEntry = document.getElementById('displayBlog')
   displayEntry.style.display = 'block'
 
-  let titleDisplay = document.createElement("H1")
+  let titleDisplay = document.createElement("H2")
   let blogTitle = document.createTextNode(titleEntry)
   titleDisplay.appendChild(blogTitle)
   displayEntry.appendChild(titleDisplay)
+
+  let contentDisplay = document.createElement("p")
+  let blogContent = document.createTextNode(contentEntry)
+  contentDisplay.appendChild(blogContent)
+  displayEntry.appendChild(contentDisplay)
 
   // let showTitle = document.getElementById('displayTitle')
   // showTitle.innerHTML = titleEntry
@@ -58,6 +86,9 @@ const postEntry = () => {
   .then(function(response){
     console.log('saved entry successfully')
   });
+
+  // clear out the entry form after posting
+  // document.getElementById('entryForm').
 
   let createPostForm = document.getElementById('entryForm')
   createPostForm.style.display = 'none'
