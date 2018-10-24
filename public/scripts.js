94f8bcd1d303fca2aa8035e287b630755e41466b
corrimori <corrine@promptt.com>
2018-10-24 13:52:20 -0700

    add async rendering to blog posts

--- public/scripts.js
+++ public/scripts.js
@@ -8,16 +8,15 @@ const loadEntries = () => {
 
   axios.get(apiUrl).then(result => {
     let data = result.data
+    console.log('data', data);
     data.forEach( element => {
       // console.log(element.title)
       let listItem = document.createElement('li')
       listItem.innerHTML = element.title
-      console.log('listitem>', listItem)
       list.appendChild(listItem).addEventListener('click', () => {
-        console.log('clicked li')
         let listId = list.id
-        console.log('element id>>>', element.id);
-        console.log('data>>>>', data)
+        // console.log('element id>>>', element.id);
+        // console.log('data>>>>', data)
         // onclick hide showEntry
         let displayBlogArea = document.getElementById('displayBlog')
         displayBlogArea.style.display = 'none'
@@ -43,9 +42,15 @@ const loadEntries = () => {
 // axios call to remove item
 
 const showForm = () => {
+  document.querySelector('#title').value = ''
+  document.querySelector('#content').value = ''
+  document.getElementById('displayBlog').innerHTML = ''
+
   console.log('in showForm function')
   let createPostForm = document.getElementById('entryForm')
   createPostForm.style.display = 'block'
+
+
   // if (createPostForm.style.display === 'none') {
   //   console.log('form>>', createPostForm.style.display);
   //   createPostForm.style.display = 'block'
@@ -55,10 +60,19 @@ const showForm = () => {
 }
 
 //function to show Blog Entry once submit button is pressed
-const showEntry = (titleEntry, contentEntry) => {
-  console.log('in showEntry function')
+const showEntry = (titleEntry, contentEntry, id) => {
   let displayEntry = document.getElementById('displayBlog')
+  displayEntry.innerHTML = ''
+  loadEntries()
+  console.log('in showEntry function')
+  document.getElementById('entryForm').style.display = 'none'
   displayEntry.style.display = 'block'
+
+  let div = document.createElement('div')
+  div.innerHTML = `<nav align="right">
+    <a id="editLink" href="#">Edit</a>
+    <a id="deleteLink" href="#">Delete</a>
+    </nav>`
 
   let titleDisplay = document.createElement("H2")
   let blogTitle = document.createTextNode(titleEntry)
@@ -68,7 +82,10 @@ const showEntry = (titleEntry, contentEntry) => {
   let contentDisplay = document.createElement("p")
   let blogContent = document.createTextNode(contentEntry)
   contentDisplay.appendChild(blogContent)
+  displayEntry.appendChild(div)
   displayEntry.appendChild(contentDisplay)
+
+
 
   // let showTitle = document.getElementById('displayTitle')
   // showTitle.innerHTML = titleEntry
@@ -87,6 +104,10 @@ const postEntry = () => {
   axios.post(apiUrl, { title: titleEntry, content: contentEntry })
   .then(function(response){
     console.log('saved entry successfully')
+    console.log('response', response);
+    let {id} = response.data[0]
+    console.log('id>>>>>', id);
+    showEntry(titleEntry, contentEntry, id)
   });
 
   // clear out the entry form after posting
@@ -95,7 +116,6 @@ const postEntry = () => {
   let createPostForm = document.getElementById('entryForm')
   createPostForm.style.display = 'none'
 
-  showEntry(titleEntry, contentEntry)
 
 }
 
